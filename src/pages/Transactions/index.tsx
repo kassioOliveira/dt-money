506fb18e28cc43a4357f42d821714c51d132cb74
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-03 17:05:37 -0300

    SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header"
+import SearchForm from "../../components/SearchForm"
 import { Summary } from "../../components/Summary"
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./style"
 
@@ -9,6 +10,7 @@ export  function Transactions() {
     <Summary/>
 
  <TransactionsContainer>
+  <SearchForm/>
  <TransactionsTable>
       <tbody>
         <tr>
