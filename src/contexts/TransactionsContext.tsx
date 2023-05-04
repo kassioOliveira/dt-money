fdef67f889c15e91aa75529242b8d39b194fde44
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-04 13:59:34 -0300

    configuring axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
+import { api } from '../lib/axios';
 
 interface Transaction {
     id:number;
@@ -28,16 +29,13 @@ export function TransactionProvider({children}: TransactionsProviderProps){
 
     async function fetchTransactions(query?: string){
         
-        const url = new URL('http://localhost:3333/transactions');
-
-        if (query) {
-            url.searchParams.append('q', query)
-        }
-        
-        const response = await fetch(url);
-        const data = await response.json();
+        const response = await api.get('transactions',{
+            params: {
+                q: query
+            }
+        })
     
-        setTransactions(data)
+        setTransactions(response.data)
     
       }
     
