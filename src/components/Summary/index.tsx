d7e1651d73705b9f0f584954d688215921234285
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-04 09:36:00 -0300

    configuring json-server

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,27 +9,24 @@ export  function Summary() {
             <header>
                 <span>Entradas</span>
                 <ArrowCircleUp size={32} color="#80b37e"/>
-
-                <strong>R$ 17.400,00</strong>
             </header>
+            <strong>R$ 17.400,00</strong>
         </SummaryCard>
 
         <SummaryCard>
             <header>
                 <span>Saídas</span>
                 <ArrowCircleDown size={32} color="#f75a68"/>
-
-                <strong>R$ 17.400,00</strong>
             </header>
+            <strong>R$ 17.400,00</strong>
         </SummaryCard>
 
         <SummaryCard variant="green">
             <header>
                 <span>Total</span>
                 <CurrencyDollar size={32} color="#fff"/>
-
-                <strong>R$ 17.400,00</strong>
             </header>
+            <strong>R$ 17.400,00</strong>
         </SummaryCard>
     </SummaryContainer>
   )
