37adda8112b5db292b9a2920ccbe3e1da5f90652
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-05 17:09:10 -0300

    dates overview

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,8 +5,14 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 import { priceFormatter } from '../../utils/fomatter'
 import { useSummary } from '../../hooks/useSummary'
 
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useTransactionDatesOverview } from '../../hooks/useTransactionDatesOverview'
+
 export function Summary() {
   const summary = useSummary()
+  const { firstIncome, lastOutcome, firstTransaction, lastTransaction } =
+    useTransactionDatesOverview()
 
   return (
     <SummaryContainer>
@@ -16,6 +22,14 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#80b37e" />
         </header>
         <strong>{priceFormatter.format(summary.income)}</strong>
+        {!!firstIncome.getTime() && (
+          <span>
+            Útima entrada em{' '}
+            {format(new Date(firstIncome), 'dd/MMMM', {
+              locale: ptBR,
+            }).replace('/', ' de ')}
+          </span>
+        )}
       </SummaryCard>
 
       <SummaryCard>
@@ -24,6 +38,14 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
         <strong>{priceFormatter.format(summary.outcome)}</strong>
+        {!!lastOutcome.getTime() && (
+          <span>
+            Útma saída em{' '}
+            {format(new Date(lastOutcome), 'dd/MMMM', {
+              locale: ptBR,
+            }).replace('/', ' de ')}
+          </span>
+        )}
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -32,6 +54,17 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
         <strong>{priceFormatter.format(summary.total)}</strong>
+
+        {!!lastOutcome.getTime() && (
+          <span>
+            {` De
+            ${format(new Date(firstTransaction), 'dd/MM/yy', {
+              locale: ptBR,
+            })} até ${format(new Date(lastTransaction), 'dd/MM/yy', {
+              locale: ptBR,
+            })} `}
+          </span>
+        )}
       </SummaryCard>
     </SummaryContainer>
   )
